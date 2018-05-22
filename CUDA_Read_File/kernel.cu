784331c168c2c8a274c0186b33e2ac56037ee075
Boštjan <bostjan@kostomaj.org>
2018-05-22 11:50:32 +0200

    Razvoj

--- CUDA_Read_File/kernel.cu
+++ CUDA_Read_File/kernel.cu
@@ -6,7 +6,6 @@ void __syncthreads();
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-
 #include <device_functions.h>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
@@ -17,7 +16,7 @@ void __syncthreads();
 #include <vector>
 
 #define SIZE 32 //512
-#define BIT
+#define BIT 32
 
 using namespace std;
 
@@ -31,62 +30,47 @@ __global__ void gpucopy(int* src, int* dst)
 	dst[i] = src[i];
 }
 
-__global__ void radixSort(int* const sort_tmp, const int num_lists, const int num_element, int* const sort_tmp_0, int* const sort_tmp_1) {
+__global__ void radixSort(int* sort_tmp, const int num_lists, const int num_element, int* sort_tmp_0, int* sort_tmp_1) {
 	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
-	for (int bit = 0; bit < 32; bit++) {
+	//printf("\n%d num list: %d" ,tid, num_lists );
+	//printf("\n num element: %d" , num_element);
+	if (sort_tmp == NULL) {
+		printf("\nsort_tmp is null");
+	}
+	
+	
+	for (int bit = 0; bit < BIT; bit++) {
 		int base_cnt_0 = 0, base_cnt_1 = 0;
-
-		for (int i = 0; i < num_element; i++) {
-
+		//printf("\ntest1 + %d",tid );
+		for (int i = 0; i < num_element; i+=num_lists) {
+			int elem = sort_tmp[i + tid];
+			const int bit_mask = (1 << bit);
+			//printf("\ntest1 + %d", elem);
+			if ((elem & bit_mask) > 0) {
+				sort_tmp_1[base_cnt_1 + tid] = elem;
+				base_cnt_1 += num_lists;
+			}
+			else {
+				sort_tmp_0[base_cnt_0 + tid] = elem;
+				base_cnt_0+=num_lists;
+			}
+		}
+		//copy data back to source - first the zero list
+		for (int i = 0; i < base_cnt_0; i += num_lists) {
+			sort_tmp[i + tid] = sort_tmp_0[i + tid];
+		}
+		//copy data back to source - then the one list
+		for (int i = 0; i < base_cnt_1; i += num_lists) {
+			sort_tmp[base_cnt_0 + i + tid] = sort_tmp_1[i + tid];
 		}
 	}
+	
+	__syncthreads();
+	/*for (int i = 0; i < num_element; i++) {
+		printf("\n%d", sort_tmp[i]);
+	}*/
 }
 
-__global__ void	ParalelRadixSort()
-{
-	int tib = (blockIdx.x * blockDim.x) + threadIdx.x;;
-	//sprememba
-	__shared__ volatile unsigned int shared_data[SIZE * 2];
-
-	//spremena za commit
-	shared_data[tib] = device_data[tib];
-		
-	unsigned int bitmask = 1 << 0;
-	unsigned int offset = 0;
-	// -1, -2, -4, -8, -16, -32, -64, -128, -256,...
-	unsigned int thrmask = 0xFFFFFFFFU << tib;
-	unsigned int mypos;
-
-	for (int i = 0; i <= SIZE; i++){
-		unsigned int mydata = shared_data[((SIZE - 1) - tib) + offset];
-		unsigned int mybit = mydata&bitmask;
-		// Get population of ones and zeroes
-		unsigned int ones = __ballot(mybit);
-		unsigned int zeroes = ~ones;
-
-		// Switch ping-pong buffers
-		offset ^= SIZE;
-
-		// Do zeroes, then ones
-		if (!mybit)
-		{
-			mypos = __popc(zeroes&thrmask);
-		}
-		else {      // Threads with a one bit
-					// Get my position in ping-pong buffer
-			mypos = __popc(zeroes) + __popc(ones&thrmask);
-		}
-
-		// Move to buffer  (or use shfl for cc 3.0)
-		shared_data[mypos - 1 + offset] = mydata;
-		// Repeat for next bit
-		bitmask <<= 1;
-
-		device_data[tib] = shared_data[tib + offset];
-	}
-		
-
-}
 
 void WriteFile(vector<double> vector) {
 	ofstream myfile;
@@ -120,8 +104,8 @@ int main(int argc, char * argv[])
 
 	std::ifstream fin(argv[1]);
 	
-	std::vector<int> host_double(9);
-	std::vector<int> host_double2(9);
+	std::vector<int> host_double(7);
+	std::vector<int> host_double2(7);
 
 	float totalTime = 0;
 	float milliseconds = 0;
@@ -140,150 +124,97 @@ int main(int argc, char * argv[])
 
 		//WriteFile(host_double);
 	}
-	/*int size = host_double.size();*/
+	int size = host_double.size();
 	
-	//int* device = NULL;
-	//int* device2 = NULL;
-	//
-	//int size = host_double.size();
+	int* device = NULL;
+	int* tmp_0 = NULL;
+	int* tmp_1 = NULL;
+	
+	cudaMalloc((void**)&device, size * sizeof(int));
+	cudaMalloc((void**)&tmp_0, size * sizeof(int));
+	cudaMalloc((void**)&tmp_1, size * sizeof(int));
 
-	//cudaMalloc((void**)&device, size * sizeof(int));
-	//cudaMalloc((void**)&device2, size * sizeof(int));
-
-	////num_list 32 
-
-	//cudaMemcpy(device_data, host_double.data(), size * sizeof(int), cudaMemcpyHostToDevice);
-	//
-	//radix_sort << <(size + size - 1) / size, size >> >();
-	////ParalelRadixSort << <(size + size - 1) / size, size >> >();
-	////gpucopy << <(size + size - 1) / size, size >> >(device, device2);
-	//
-	//cudaDeviceSynchronize();
-
-	//cudaMemcpy(host_double.data(), device_data, size * sizeof(int), cudaMemcpyDeviceToHost);
-
-	//
-
-	//cout << endl << "host data" << endl;
-	//for each (int var in host_double)
-	//{
-	//	double d = (double)var / 10;
-	//	cout << d << " " << endl;
-	//}
-
-	//cudaFree(device);
-
-	std::vector<int> v(10);
-
-	unsigned int hdata[SIZE];
-
-
-		int size = host_double.size();
-		//vector.resize(size);
-
-		std::copy(host_double.begin(), host_double.end(), hdata);
-		//int *a = vector.data();
-
-		// Copy data from host to device
-		cudaMemcpyToSymbol(device_data, hdata, SIZE * sizeof(unsigned int));
-
-		// Execution time measurement, that point starts the clock
-		cudaEventRecord(start);
-		ParalelRadixSort << <1, SIZE>> >();
-
-		// Execution time measurement, that point stops the clock
-		cudaEventRecord(stop);
-
-		// Make kernel function synchronous
-		cudaDeviceSynchronize();
-
-		cudaEventElapsedTime(&milliseconds, start, stop);
-		totalTime += milliseconds;
-		//milliseconds = 0;
-
-		// Copy data from device to host
-		cudaMemcpyFromSymbol(hdata, device_data, SIZE * sizeof(unsigned int));
-
-		/*std::vector<int> v(hdata, hdata + sizeof hdata / sizeof hdata[0]);
-		v.resize(size);*/
-		milliseconds = 0;
+	int* a = host_double.data();
 	
 
-	/*printf("TIME %4.2fs", milliseconds);
-	printf("\n");
-	printf("Effective Bandwidth (GB/s): %fn", SIZE * 4 * 3 / milliseconds / 1e6);
-	printf("\n");*/
-	cout << endl;
-	cout << "Host Data" << endl;
-	/*for each (int var in v)
+	//num_list 32 
+
+	cudaMemcpy(device,&*host_double.data(), size * sizeof(int), cudaMemcpyHostToDevice);
+	
+	radixSort << < 1,SIZE >> >(device, SIZE, size, tmp_0, tmp_1);
+	//ParalelRadixSort << <(size + size - 1) / size, size >> >();
+	//gpucopy << <1,SIZE>> >(device, device2);
+	
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(&*host_double.data(), device, size * sizeof(int), cudaMemcpyDeviceToHost);
+
+	cout << endl << "host data" << endl;
+	for each (int var in host_double)
 	{
 		double d = (double)var / 100;
 		cout << d << " " << endl;
-	}*/
-	for (int i = 0; i < SIZE; i++) {
-		double d = (double)hdata[i] / 100;
-		cout << d << endl;
 	}
-	
-	cout << endl;
-	cout << "TIME %4.2fs " << totalTime << endl;
+
+	cudaFree(device);
+	cudaFree(tmp_0);
+	cudaFree(tmp_1);
+
+	//std::vector<int> v(10);
+
+	//unsigned int hdata[SIZE];
+
+
+	//	int size = host_double.size();
+	//	//vector.resize(size);
+
+	//	std::copy(host_double.begin(), host_double.end(), hdata);
+	//	//int *a = vector.data();
+
+	//	// Copy data from host to device
+	//	cudaMemcpyToSymbol(device_data, hdata, SIZE * sizeof(unsigned int));
+
+	//	// Execution time measurement, that point starts the clock
+	//	cudaEventRecord(start);
+	//	ParalelRadixSort << <1, SIZE>> >();
+
+	//	// Execution time measurement, that point stops the clock
+	//	cudaEventRecord(stop);
+
+	//	// Make kernel function synchronous
+	//	cudaDeviceSynchronize();
+
+	//	cudaEventElapsedTime(&milliseconds, start, stop);
+	//	totalTime += milliseconds;
+	//	//milliseconds = 0;
+
+	//	// Copy data from device to host
+	//	cudaMemcpyFromSymbol(hdata, device_data, SIZE * sizeof(unsigned int));
+
+	//	/*std::vector<int> v(hdata, hdata + sizeof hdata / sizeof hdata[0]);
+	//	v.resize(size);*/
+	//	milliseconds = 0;
+	//
+
+	///*printf("TIME %4.2fs", milliseconds);
+	//printf("\n");
+	//printf("Effective Bandwidth (GB/s): %fn", SIZE * 4 * 3 / milliseconds / 1e6);
+	//printf("\n");*/
+	//cout << endl;
+	//cout << "Host Data" << endl;
+	///*for each (int var in v)
+	//{
+	//	double d = (double)var / 100;
+	//	cout << d << " " << endl;
+	//}*/
+	//for (int i = 0; i < SIZE; i++) {
+	//	double d = (double)hdata[i] / 100;
+	//	cout << d << endl;
+	//}
+	//
+	//cout << endl;
+	//cout << "TIME %4.2fs " << totalTime << endl;
 
     return 0;
 }
 
-
-
-
-
-
-
-//
-//#define nTPB 512
-//
-//__global__ void gpucopy(int* src, int* dst)
-//{
-//	int i = blockIdx.x * blockDim.x + threadIdx.x;;
-//	dst[i] = src[i];
-//}
-//
-//int main()
-//{
-//	const int arraySize = 500; // >= 1025 will fail on my system!
-//
-//	int* data1 = new int[arraySize];
-//	int* data2 = new int[arraySize];
-//	// Initialized both data1 and data2
-//	// ... 
-//	for (int i = 0; i < arraySize; i++) {
-//		data1[i] = 2 * i;
-//		//cout << data1[i] + " ";
-//	}
-//		
-//
-//	int* dev_data1 = NULL;
-//	int* dev_data2 = NULL;
-//	// Initialized both dev_data1 and dev_data2
-//	// ... 
-//	cudaMalloc(&dev_data1, arraySize * sizeof(int));
-//	cudaMalloc(&dev_data2, arraySize * sizeof(int));
-//
-//	// copy data1 to device
-//	cudaMemcpy(dev_data1, data1, arraySize * sizeof(int), cudaMemcpyHostToDevice);
-//
-//	//// copy dev_data1 to dev_data2 with gpu
-//	//gpucopy << <1, arraySize >> >(dev_data1, dev_data2);
-//	gpucopy << <(arraySize + nTPB - 1) / nTPB, nTPB >> >(dev_data1, dev_data2);
-//
-//	// copy dev_data2 to data
-//	cudaMemcpy(data2, dev_data2, arraySize * sizeof(int), cudaMemcpyDeviceToHost);
-//
-//
-//	for (int i = 0; i<arraySize; i++)
-//		if (data2[i] != data1[i])
-//			cout << "Error: data is different - data2[" << i << "] is " << data2[i] << endl;
-//
-//	return 0;
-//}
-//
-//
-//
