fc8a87579f4904d701a8d91daf1a30f0a9cf324f
Boštjan <bostjan@kostomaj.org>
2018-05-23 23:09:40 +0200

    Razvoj

--- CUDA_Read_File/kernel.cu
+++ CUDA_Read_File/kernel.cu
@@ -14,9 +14,9 @@ void __syncthreads();
 #include <fstream>
 #include <vector>
 #include <bitset>
-//
-//#define SIZE 5
-//#define BIT 5
+
+//#define SIZE 32
+//#define BIT 32
 //
 using namespace std;
 //
@@ -26,12 +26,20 @@ using namespace std;
 ////Test v gpu in nazaj
 //__global__ void gpucopy(int* src, int* dst)
 //{
-//	int i = (blockIdx.x * blockDim.x) + threadIdx.x;
-//	dst[i] = src[i];
+//	int index = blockIdx.x * blockDim.x + threadIdx.x;
+//	int stride = blockDim.x * gridDim.x;
+//	for (int i = index; i < SIZE; i += stride) {
+//		if(src[i] > 50)
+//			src[i] = dst[i] + 100;
+//		else 
+//			src[i] = dst[i] - 100;
+//	}
+//		
 //}
 //
 //__global__ void radixSort(int* sort_tmp, const int num_lists, const int num_element, int* sort_tmp_1) {
 //	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
+//	int stride = blockDim.x * gridDim.x;
 //	//printf("\n%d num list: %d" ,tid, num_lists );
 //	//printf("\n num element: %d" , num_element);
 //	if (sort_tmp == NULL) {
@@ -39,20 +47,20 @@ using namespace std;
 //	}
 //	
 //	
-//	for (int bit = 0; bit < BIT; bit++) {
+//	//for (int bit = 0; bit < BIT; bit++) {
 //		int base_cnt_0 = 0, base_cnt_1 = 0;
 //		//printf("\ntest1 + %d",tid );
-//		for (int i = 0; i < num_element; i+=num_lists) {
-//			int elem = sort_tmp[i + tid];
-//			const int bit_mask = (1 << bit);
+//		for (int i = tid; i < num_element; i+=stride) {
+//			int elem = sort_tmp[i];
+//			const int bit_mask = (1 << i);
 //			//printf("\ntest %d + %d bit %d",tid, elem, bit_mask);
 //			if ((elem & bit_mask) > 0) {
-//				sort_tmp_1[base_cnt_1 + tid] = elem;
+//				sort_tmp_1[base_cnt_1] = elem;
 //				base_cnt_1 += num_lists;
 //				//printf("\n element 1 %d = %d",i, elem);
 //			}
 //			else {
-//				sort_tmp[base_cnt_0 + tid] = elem;
+//				sort_tmp[base_cnt_0] = elem;
 //				base_cnt_0+=num_lists;
 //				//printf("\n element 0 %d = %d",i, elem);
 //			}
@@ -62,7 +70,7 @@ using namespace std;
 //			sort_tmp[base_cnt_0 + i + tid] = sort_tmp_1[i + tid];
 //			//printf("\nsort tmp: %d", sort_tmp_0[i + tid]);
 //		}
-//	}
+//	//}
 //	
 //	__syncthreads();
 //
@@ -86,9 +94,8 @@ vector<unsigned int> ReadFile(string filepath) {
 	ifstream inputFile(filepath);
 	double value;
 
-	// read the elements in the file into a vector  
+	 //read the elements in the file into a vector  
 	while (inputFile >> value) {
-		//string binary = bitset<16>(value * 100).to_string();
 		vector.push_back(value * 100);
 	}
 	for each (unsigned int var in vector)
@@ -105,7 +112,7 @@ vector<unsigned int> ReadFile(string filepath) {
 //
 //	std::ifstream fin(argv[1]);
 //	
-//	std::vector<string> host_double(7);
+//	std::vector<unsigned int> host_double(7);
 //	std::vector<int> host_double2(7);
 //
 //	float totalTime = 0;
@@ -135,9 +142,8 @@ vector<unsigned int> ReadFile(string filepath) {
 //	cudaMalloc((void**)&tmp_0, size * sizeof(int));
 //	cudaMalloc((void**)&tmp_1, size * sizeof(int));
 //
-//	string s = host_double[0];
-//	cout << "test: " << s[0] << endl;
-//	
+//	int blockSize = 4;
+//	int numBlocks = (SIZE + blockSize - 1) / blockSize;
 //	
 //	//string binary = bitset<16>(222).to_string(); //to binary
 //	//cout << binary << "\n";	
@@ -149,20 +155,20 @@ vector<unsigned int> ReadFile(string filepath) {
 //
 //	cudaMemcpy(device,&*host_double.data(), size * sizeof(int), cudaMemcpyHostToDevice);
 //	
-//	//radixSort << < 1,5>> >(device, SIZE, size, tmp_1);
+//	//radixSort << <numBlocks, blockSize>> >(device, SIZE, size, tmp_1);
 //	//ParalelRadixSort << <(size + size - 1) / size, size >> >();
-//	//gpucopy << <1,SIZE>> >(device, device2);
+//	gpucopy << <numBlocks, blockSize>> >(device, device);
 //	
 //	cudaDeviceSynchronize();
 //
 //	cudaMemcpy(&*host_double.data(), device, size * sizeof(int), cudaMemcpyDeviceToHost);
 //
-//	//cout << endl << "host data" << endl;
-//	//for each (string var in host_double)
-//	//{
-//	//	double d = (double)var / 100;
-//	//	cout << d << " " << endl;
-//	//}
+//	cout << endl << "host data" << endl;
+//	for each (unsigned int var in host_double)
+//	{
+//		double d = (double)var / 100;
+//		cout << d << " " << endl;
+//	}
 //
 //	cudaFree(device);
 //	cudaFree(tmp_0);
@@ -225,35 +231,38 @@ vector<unsigned int> ReadFile(string filepath) {
 //
 //    return 0;
 //}
-//
 
 
-#define WSIZE 32
+
+#define SIZE 32
 #define LOOPS 1
 #define UPPER_BIT 31
 #define LOWER_BIT 0
 
-__device__ unsigned int ddata[WSIZE];
+__device__ unsigned int device_data[SIZE];
 
 // naive warp-level bitwise radix sort
 
-__global__ void mykernel() {
-	__shared__ volatile unsigned int sdata[WSIZE * 2];
+__global__ void radixSort() {
+	__shared__ volatile unsigned int shared_data[SIZE * 2];
 	// load from global into shared variable
 	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
-	sdata[tid] = ddata[tid];
+
+	shared_data[tid] = device_data[tid];
 	unsigned int bitmask = 1 << LOWER_BIT;
 	unsigned int offset = 0;
 	unsigned int thrmask = 0xFFFFFFFFU << tid;
 	unsigned int mypos;
+
 	//  for each LSB to MSB
 	for (int i = LOWER_BIT; i <= UPPER_BIT; i++) {
-		unsigned int mydata = sdata[((WSIZE - 1) - tid) + offset];
+		/*unsigned int mydata = shared_data[((SIZE - 1) - tid) + offset];*/
+		unsigned int mydata = shared_data[((SIZE - 1) - tid) + offset];
 		unsigned int mybit = mydata&bitmask;
 		// get population of ones and zeroes (cc 2.0 ballot)
 		unsigned int ones = __ballot(mybit); // cc 2.0
 		unsigned int zeroes = ~ones;
-		offset ^= WSIZE; // switch ping-pong buffers
+		offset ^= SIZE; // switch ping-pong buffers
 						 // do zeroes, then ones
 		if (!mybit) // threads with a zero bit
 					// get my position in ping-pong buffer
@@ -262,42 +271,55 @@ __global__ void mykernel() {
 					// get my position in ping-pong buffer
 			mypos = __popc(zeroes) + __popc(ones&thrmask);
 		// move to buffer  (or use shfl for cc 3.0)
-		sdata[mypos - 1 + offset] = mydata;
+		shared_data[mypos - 1 + offset] = mydata;
 		// repeat for next bit
 		bitmask <<= 1;
 	}
 	// save results to global
-	ddata[tid] = sdata[tid + offset];
+	device_data[tid] = shared_data[tid + offset];
+	
+
 }
 
 int main(int argc, char * argv[]) {
+
+	int blockSize = 32;
+	int numBlocks = (SIZE + blockSize - 1) / blockSize;
+
+
 	std::ifstream fin(argv[1]);
-	std::vector<unsigned int> host_double(WSIZE);
+	std::vector<unsigned int> host_double(SIZE);
 
-	unsigned int hdata[WSIZE];
+	unsigned int host_data[SIZE];
 
-	
-
-	//if (!fin) {
-	//	cerr << "Datoteka ne obstaja " << argv[1] << endl;
-	//}
-	//else {
-	//	cout << "Datoteka obstaja " << argv[1] << endl;
-	//	host_double = ReadFile(argv[1]);
-	//	
-	//	//WriteFile(host_double);
-	//}
-	//std::copy(host_double.begin(), host_double.end(), hdata);
+	if (!fin) {
+		cerr << "Datoteka ne obstaja " << argv[1] << endl;
+	}
+	else {
+		cout << "Datoteka obstaja " << argv[1] << endl;
+		host_double = ReadFile(argv[1]);
+		
+		//WriteFile(host_double);
+	}
+	int size = host_double.size();
+	std::copy(host_double.begin(), host_double.end(), host_data);
 
 	for (int lcount = 0; lcount < LOOPS; lcount++) {
 		unsigned int range = 1U << UPPER_BIT;
-		for (int i = 0; i < WSIZE; i++) hdata[i] = rand() % range;
-		cudaMemcpyToSymbol(ddata, hdata, WSIZE * sizeof(unsigned int));
-		mykernel << <1, 32 >> >();
-		cudaMemcpyFromSymbol(hdata, ddata, WSIZE * sizeof(unsigned int));
-		for (int i = 0; i < WSIZE - 1; i++) if (hdata[i] > hdata[i + 1]) { printf("sort error at loop %d, hdata[%d] = %d, hdata[%d] = %d\n", lcount, i, hdata[i], i + 1, hdata[i + 1]); return 1; }
-		 printf("sorted data:\n");
-		for (int i = 0; i < WSIZE; i++) cout << (double)hdata[i] / 100 << endl;
+		//for (int i = 0; i < SIZE; i++) host_data[i] = rand() % range;
+		cudaMemcpyToSymbol(device_data, host_data, SIZE *  sizeof(unsigned int));
+		radixSort << <numBlocks,blockSize >> >();
+		cudaMemcpyFromSymbol(host_data, device_data, SIZE * sizeof(unsigned int));
+	/*for (int i = 0; i < SIZE - 1; i++) {
+			if (host_data[i] > host_data[i + 1]) { 
+				printf("sort error at loop %d, hdata[%d] = %d, hdata[%d] = %d\n", lcount, i, host_data[i], i + 1, host_data[i + 1]);
+				return 1; 
+			}
+		}*/
+		printf("sorted data:\n");
+		for (int i = 0; i < size; i++) {
+			cout << (double)host_data[i] / 100 << endl;
+		}
 	}
 	printf("Success!\n");
 	return 0;
