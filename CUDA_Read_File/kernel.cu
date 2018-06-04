982cbbdb648b5b16c1a2b792c49ad4e9bc282bd5
Boštjan <bostjan@kostomaj.org>
2018-06-04 21:15:43 +0200

    Razvoj.

--- CUDA_Read_File/kernel.cu
+++ CUDA_Read_File/kernel.cu
@@ -27,10 +27,11 @@ void __syncthreads();
 #include <time.h>
 #include <chrono>
 #include "device_functions.h"
+#include <math.h> 
 
 
 //#define SIZE 32
-//#define BIT 32
+#define BIT 32
 //
 using namespace std;
 using namespace chrono;
@@ -52,47 +53,7 @@ using namespace chrono;
 //		
 //}
 //
-__global__ void radixSort(int* sort_tmp, const int num_lists, const int num_element, int* sort_tmp_1) {
-	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
-	int stride = blockDim.x * gridDim.x;
-	//printf("\n%d num list: %d" ,tid, num_lists );
-	//printf("\n num element: %d" , num_element);
-	if (sort_tmp == NULL) {
-		printf("\nsort_tmp is null");
-	}
-	
-	
-	//for (int bit = 0; bit < BIT; bit++) {
-		int base_cnt_0 = 0, base_cnt_1 = 0;
-		//printf("\ntest1 + %d",tid );
-		for (int i = tid; i < num_element; i+=stride) {
-			int elem = sort_tmp[i];
-			const int bit_mask = (1 << i);
-			//printf("\ntest %d + %d bit %d",tid, elem, bit_mask);
-			if ((elem & bit_mask) > 0) {
-				sort_tmp_1[base_cnt_1] = elem;
-				base_cnt_1 += num_lists;
-				//printf("\n element 1 %d = %d",i, elem);
-			}
-			else {
-				sort_tmp[base_cnt_0] = elem;
-				base_cnt_0+=num_lists;
-				//printf("\n element 0 %d = %d",i, elem);
-			}
-		}
-		//copy data back to source - first the zero list
-		for (int i = 0; i < base_cnt_1; i += num_lists) {
-			sort_tmp[base_cnt_0 + i + tid] = sort_tmp_1[i + tid];
-			//printf("\nsort tmp: %d", sort_tmp_0[i + tid]);
-		}
-	//}
-	
-	__syncthreads();
 
-	for (int i = 0; i < num_element; i++) {
-		printf("\n%d", sort_tmp[i]);
-	}
-}
 //
 //
 //void WriteFile(vector<double> vector) {
@@ -104,22 +65,7 @@ __global__ void radixSort(int* sort_tmp, const int num_lists, const int num_elem
 //	}
 //	myfile.close();
 //}
-vector<unsigned int> ReadFile(string filepath) {
-	vector<unsigned int> vector;
-	ifstream inputFile(filepath);
-	double value;
 
-	 //read the elements in the file into a vector  
-	while (inputFile >> value) {
-		vector.push_back(value * 100);
-	}
-	/*for each (unsigned int var in vector)
-	{
-		cout << var << " " << endl;
-	}*/
-	inputFile.close();
-	return vector;
-}
 //
 //int main(int argc, char * argv[])
 //{
@@ -249,7 +195,7 @@ vector<unsigned int> ReadFile(string filepath) {
 
 
 
-#define SIZE 32
+#define SIZE (33 * 1024)
 #define LOOPS 1
 #define UPPER_BIT 31
 #define LOWER_BIT 0
@@ -313,45 +259,63 @@ void serialRadixSort(unsigned int* arr, int n)
 __device__ unsigned int device_data[SIZE];
 
 // naive warp-level bitwise radix sort
+__global__ void radixSortParralel(unsigned int* sort_tmp, unsigned int* sort_tmp_1) {
+	const int num_lists = SIZE;
+	const int num_element = 10;
 
-__global__ void radixSort() {
-	__shared__ volatile unsigned int shared_data[SIZE * 2];
-	// load from global into shared variable
-	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
-
-	shared_data[tid] = device_data[tid];
-	unsigned int bitmask = 1 << LOWER_BIT;
-	unsigned int offset = 0;
-	unsigned int thrmask = 0xFFFFFFFFU << tid;
-	unsigned int mypos;
-
-	//  for each LSB to MSB
-	for (int i = LOWER_BIT; i <= UPPER_BIT; i++) {
-		/*unsigned int mydata = shared_data[((SIZE - 1) - tid) + offset];*/
-		unsigned int mydata = shared_data[((SIZE - 1) - tid) + offset];
-		//unsigned int mydata = shared_data[((SIZE - 1) - tid)];
-		unsigned int mybit = mydata&bitmask;
-		// get population of ones and zeroes (cc 2.0 ballot)
-		unsigned int ones = __ballot(mybit); // cc 2.0
-		unsigned int zeroes = ~ones;
-		//offset ^= SIZE; // switch ping-pong buffers
-						 // do zeroes, then ones
-		if (!mybit) // threads with a zero bit
-					// get my position in ping-pong buffer
-			mypos =		__popcll(zeroes&thrmask);
-		else        // threads with a one bit
-					// get my position in ping-pong buffer
-			mypos = __popcll(zeroes) + __popcll(ones&thrmask);
-		// move to buffer  (or use shfl for cc 3.0)
-		shared_data[mypos - 1 + offset] = mydata;
-		//shared_data[mypos - 1] = mydata;
-		// repeat for next bit
-		bitmask <<= 1;
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	int stride = blockDim.x * gridDim.x;   
+	//printf("\n%d num list: %d" ,tid, num_lists );
+	//printf("\n num element: %d" , num_element);
+	if (sort_tmp == NULL) {
+		printf("\nsort_tmp is null");
 	}
-	// save results to global
-	device_data[tid] = shared_data[tid + offset];
-	//device_data[tid] = shared_data[tid];
 	
+
+    for (int bit = 0; bit < BIT; bit++) {
+		int base_cnt_0 = 0, base_cnt_1 = 0;
+		//printf("\ntest1 + %d",tid );
+		for (int i = tid; i < num_element; i += stride) {
+			int elem = sort_tmp[i];
+			const int bit_mask = (1 << i);
+			//printf("\ntest %d + %d bit %d",tid, elem, bit_mask);
+			if ((elem & bit_mask) > 0) {
+				sort_tmp_1[base_cnt_1] = elem;
+				base_cnt_1 += num_lists;
+				//printf("\n element 1 %d = %d",i, elem);
+			}
+			else {
+				sort_tmp[base_cnt_0] = elem;
+				base_cnt_0 += num_lists;
+				//printf("\n element 0 %d = %d",i, elem);
+			}
+		}
+		//copy data back to source - first the zero list
+		for (int i = 0; i < base_cnt_1; i += num_lists) {
+			sort_tmp[base_cnt_0 + i + tid] = sort_tmp_1[i + tid];
+			//printf("\nsort tmp: %d", sort_tmp_0[i + tid]);
+		}
+	}
+
+	__syncthreads();
+
+	//for (int i = 0; i < num_element; i++) {
+	//	printf("\n%d", sort_tmp[i]);
+	//}
+}
+
+__global__ void radixSort(unsigned int* data, unsigned int* data_tmp) {
+	//__shared__ volatile unsigned int shared_data[SIZE * 2];
+	// load from global into shared variable
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+	while (tid < SIZE) {
+		//data[tid] = data[tid] + 1000;
+
+
+
+		tid += blockDim.x * gridDim.x;
+	}
 
 }
 void print(unsigned int* arr, int n)
@@ -365,6 +329,63 @@ void menu() {
 	cout << "0 Exit " << endl;
 	cout << " 1 Serial sort" << endl;
 	cout << " 2 Parrallel sort" << endl;
+}
+// function to convert decimal to binary
+unsigned int decToBinary(int n)
+{
+	// array to store binary number
+	unsigned int binaryNum[32];
+	vector<int> binary;
+	
+
+	// counter for binary array
+	int i = 0;
+	while (n > 0) {
+
+	//	// storing remainder in binary array
+		binary.push_back(n % 2);
+		n = n / 2;
+		i++;
+	}
+	cout << "binary" << endl;
+	for (int i = 0; i < binary.size(); i++) {
+		cout <<  binary[i] << " ";
+	}
+	cout << endl;
+	//binary = { 1,1,0 };
+
+	int total = 0;
+	int multipicator = 1;
+	for (int i = 0; i < binary.size(); i++)
+	{
+	   total += binary[i] * multipicator;
+	   multipicator *= 10;
+	}
+	return total;
+
+	// printing binary array in reverse order
+	/*for (int j = i - 1; j >= 0; j--)
+		cout << binaryNum[j];*/
+}
+
+vector<unsigned int> ReadFile(string filepath) {
+	vector<unsigned int> vector;
+	ifstream inputFile(filepath);
+	double value;
+
+	//read the elements in the file into a vector  
+	while (inputFile >> value) {
+		bitset<32>tmp(value *100);
+		cout << tmp << " ";
+		vector.push_back((int)(tmp.to_ulong()));
+		cout << (int)(tmp.to_ulong()) << " "; // konvert nazaj to int
+	}
+	/*for each (unsigned int var in vector)
+	{
+	cout << var << " " << endl;
+	}*/
+	inputFile.close();
+	return vector;
 }
 
 int main(int argc, char * argv[]) {
@@ -429,31 +450,51 @@ int main(int argc, char * argv[]) {
 	else {
 
 		//Parrallel radix sort
-
+		cout << endl;
+		//unsigned int t = decToBinary(10);
+		//unsigned int tmp22;
+		/*bitset<32>tmp(6560);
+		cout << tmp << endl;*/
+			
 		cout << "Parrallel sort " << endl;
 		std::copy(host_double.begin(), host_double.end(), host_data);
+		cout << "Not sorted!" << endl;
+		print(host_data, size);
 
-		for (int lcount = 0; lcount < LOOPS; lcount++) {
-			unsigned int range = 1U << UPPER_BIT;
-			//for (int i = 0; i < SIZE; i++) host_data[i] = rand() % range;
-			cudaMemcpyToSymbol(device_data, host_data, SIZE * sizeof(unsigned int));
-			high_resolution_clock::time_point start = high_resolution_clock::now();
-			radixSort << <numBlocks, blockSize >> >();
-			high_resolution_clock::time_point stop = high_resolution_clock::now();
-			cudaMemcpyFromSymbol(host_data, device_data, SIZE * sizeof(unsigned int));
-			auto durationParralel = duration_cast <milliseconds> (start - stop).count();
-			/*for (int i = 0; i < SIZE - 1; i++) {
-			if (host_data[i] > host_data[i + 1]) {
-			printf("sort error at loop %d, hdata[%d] = %d, hdata[%d] = %d\n", lcount, i, host_data[i], i + 1, host_data[i + 1]);
-			return 1;
-			}
-			}*/
-			cout << " Sorted data: " << endl;
-			for (int i = 0; i < size; i++) {
-				cout << (double)host_data[i] / 100 << endl;
-			}
-			cout << " Time parralel sort: " << durationParralel << endl;
-		}
+		//unsigned int* ddata;
+		//unsigned int* ddata_tmp;
+		//
+
+		//for (int lcount = 0; lcount < LOOPS; lcount++) {
+		//	unsigned int range = 1U << UPPER_BIT;
+		//	//for (int i = 0; i < SIZE; i++) host_data[i] = rand() % range;
+		//	//cudaMemcpyToSymbol(device_data, host_data, SIZE * sizeof(unsigned int));
+		//	cudaMalloc((void**)&ddata_tmp, SIZE * sizeof(unsigned int));
+		//	cudaMalloc((void**)&ddata, SIZE * sizeof(unsigned int));
+		//	cudaMemcpy(ddata, host_data, SIZE * sizeof(unsigned int), cudaMemcpyHostToDevice);
+		//	
+
+		//	high_resolution_clock::time_point start = high_resolution_clock::now();
+		//	radixSort << <numBlocks, blockSize >> >(ddata, ddata_tmp);
+		//	//radixSort << <(numBlocks, blockSize) >> > (ddata, ddata_tmp);
+		//	high_resolution_clock::time_point stop = high_resolution_clock::now();
+
+		//	cudaMemcpy(host_data, ddata, SIZE * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+
+		//	//cudaMemcpyFromSymbol(host_data, device_data, SIZE * sizeof(unsigned int));
+		//	auto durationParralel = duration_cast <milliseconds> (start - stop).count();
+		//	/*for (int i = 0; i < SIZE - 1; i++) {
+		//	if (host_data[i] > host_data[i + 1]) {
+		//	printf("sort error at loop %d, hdata[%d] = %d, hdata[%d] = %d\n", lcount, i, host_data[i], i + 1, host_data[i + 1]);
+		//	return 1;
+		//	}
+		//	}*/
+		//	cout << " Sorted data: " << endl;
+		//	for (int i = 0; i < size; i++) {
+		//		cout << i << "     " <<(double)host_data[i] / 100 << endl;
+		//	}
+		//	cout << " Time parralel sort: " << durationParralel << endl;
+		//}
 		
 	}
 	
